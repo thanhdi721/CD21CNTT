059579c7a44ad19ac6138481dde48940767a8f52
thong2085 <129254706+thong2085@users.noreply.github.com>
2023-09-14 18:45:31 +0700

    Add sidebarleft and add function delete width restore

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
 const Schema = mongoose.Schema;
 
-mongoose.plugin(slug);
+
 const Course = new Schema({
     name: { type: String, require: true },
     des: { type: String, },
@@ -10,8 +11,14 @@ const Course = new Schema({
     videoId: { type: String, require: true },
     level: { type: String, },
     slug: { type: String, slug: 'name', unique: true },
-},{
+}, {
     timestamps: true,
+});
+// Add plugin
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, {
+    deletedAt: true,
+    overrideMethods: 'all',
 });
 
 module.exports = mongoose.model('Course', Course);
--- src/routes/me.js
+++ src/routes/me.js
@@ -5,6 +5,7 @@ const meController = require('../app/controllers/MeController');
 
 // meController.index
 router.get('/stored/courses', meController.storedCourses);
+router.get('/trash/courses', meController.trashCourses);
 
 
 module.exports = router;
